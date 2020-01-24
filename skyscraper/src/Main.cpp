66be85f9d4ff8c24e8dcb5709d5d1a887ee82efc
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-01-24 15:35:55 +0100

    SFML

--- skyscraper/src/Main.cpp
+++ skyscraper/src/Main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char** argv) {
-  for (int i = 0; i < argc; ++i) {
-    std::cout << argv[i] << "\n";
+int main()
+{
+  sf::ContextSettings context_settings;
+  context_settings.antialiasingLevel = 16;
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", 7, context_settings);
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Yellow);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear(sf::Color::Blue);
+    window.draw(shape);
+    window.display();
   }
+
   return 0;
 }
